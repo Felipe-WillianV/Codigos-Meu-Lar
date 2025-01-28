8910b54a86d9cad9b617adbe1c6996f68728d7da
Felipe-WillianV <felipewillianvieira78@gmail.com>
2025-01-28 09:31:19 -0300

    Atualização de Link Web

--- meular.js
+++ meular.js
@@ -2,8 +2,14 @@ function isMobile() {
     return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 }
 
-var whatsappLink = document.getElementById('whatsapp-link');
-
-if (isMobile()) {
-    whatsappLink.href = "whatsapp://send?phone=5511920197375";
-}
+document.addEventListener('DOMContentLoaded', function() {
+    var whatsappLink = document.getElementById('whatsapp-link');
+    
+    if (whatsappLink) {
+        if (isMobile()) {
+            whatsappLink.href = "whatsapp://send?phone=5511920197375";
+        } else {
+            whatsappLink.href = "https://web.whatsapp.com/send?phone=5511920197375";
+        }
+    }
+});
